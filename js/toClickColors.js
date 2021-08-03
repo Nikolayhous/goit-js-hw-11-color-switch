a8fba802a4a06419f823fefe47167a21ce220de8
Николай Гребенюк <Yanukhous@ukr.net>
2021-08-03 19:43:29 +0300

    Добавил скрипт для переключения цветов с интервалом времени

---
+++ js/toClickColors.js
@@ -0,0 +1,64 @@
+import { colors } from './main-colors.js';
+
+
+const refs = {
+    btnStart: document.querySelector('[data-action="start"]'),
+    btnStop: document.querySelector('[data-action="stop"]'),
+    body: document.body,
+};
+
+const DELAY = 1000;
+let timerId = null;
+
+
+refs.btnStart.addEventListener('click', toClickStartColors);
+refs.btnStop.addEventListener('click', onClickStopColors);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const randomIntegerFromInterval = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   };
+
+//  function toClickStartColors() {
+//     timerId = setInterval(() => {
+//     refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+// }, DELAY);
+//     refs.btnStart.disabled = true;
+
+// };
+
+// function onClickStopColors() {
+//  clearInterval(timerId);
+//  refs.btnStart.disabled = false;
+// };
+
+
+
+
+
+
+
+  
+

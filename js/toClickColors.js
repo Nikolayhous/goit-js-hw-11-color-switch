e41217bdedcc7f60440779b957e550b992c068e4
Николай Гребенюк <Yanukhous@ukr.net>
2021-08-09 19:33:20 +0300

    Добавив стилі  та класи через js  на кнопки

--- js/toClickColors.js
+++ js/toClickColors.js
@@ -22,12 +22,17 @@ refs.btnStop.addEventListener('click', onClickStopColors);
  function toClickStartColors() {
     timerId = setInterval(() => {
     refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    refs.btnStop.classList.add('btnStop');
+    refs.btnStart.classList.remove('btnStart');
 }, DELAY);
-    refs.btnStart.disabled = true;
 
+    refs.btnStart.disabled = true;
 };
 
 function onClickStopColors() {
+  refs.btnStart.classList.add('btnStart');
+  refs.btnStop.classList.remove('btnStop');
+  refs.body.style.backgroundColor = '';
  clearInterval(timerId);
  refs.btnStart.disabled = false;
 };
